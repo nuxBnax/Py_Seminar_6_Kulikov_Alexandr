80924fbf828bc0cbc3117632ed94cd1e367f2128
Kulikov Alexander <shouter84@yandex.ru>
2023-07-31 22:32:33 +0300

    Added solution for task_32

--- task_32.py
+++ task_32.py
@@ -4,9 +4,19 @@
 # (т.е. не меньше заданного минимума и не больше 
 # заданного максимума)
 
-# Ввод: [-5, 9, 0, 3, -1, -2, 1,
-# 4, -2, 10, 2, 0, -9, 8, 10, -9,
-# 0, -5, -5, 7]
+# Ввод: [-5, 9, 0, 3, -1, -2, 1, 4, -2, 10, 2, 0, -9, 8, 10, -9, 0, -5, -5, 7]
 # 5
 # 15
 # Вывод: [1, 9, 13, 14, 19]
+from random import randint
+
+size_ = int(input('Введите размер списка: '))
+list_ = [randint(-10, 10) for i in range(size_)]
+border = list(map(int, (input("Введите искомый диапазон чисел: ").split())))
+print(border)
+print(list_)
+new_list = []
+for i in range(len(list_)):
+    if border[0] <= list_[i] <= border[1]:
+        new_list.append(list_[i])
+print(new_list)
